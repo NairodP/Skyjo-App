8b4e73ea7ab7705946f9e5163621925109e7a12d
Dorian <dorianpernot2@gmail.com>
2024-11-10 15:11:44 +0100

    supp margin footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="font-zillaSlab mt-4 w-full max-w-90 mx-auto p-4 text-center text-xs text-black bg-sky-50 border-t border-gray-200 shadow-[rgba(0,0,10,0.1)_0px_-10px_10px_1px]">
+    <footer className="font-zillaSlab mt-0 w-full max-w-90 mx-auto p-4 text-center text-xs text-black bg-sky-50 border-t border-gray-200 shadow-[rgba(0,0,10,0.1)_0px_-10px_10px_1px]">
       <div className="space-y-2">
         <p className="font-semibold">
           © {new Date().getFullYear()} - Dorian Pernot. Tous droits réservés.
